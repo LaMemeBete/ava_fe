9eb7b5b1f61d3ad7d3ee601b75b2c4e8ea061ed5
Moshe Borochov <info@lamemebete.xyz>
2022-04-28 21:42:44 -0400

    infinite scroll support for stackblitz

--- src/components/CryptosContainer.js
+++ src/components/CryptosContainer.js
@@ -1,34 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { connect } from 'react-redux'
 import { fetchCryptos } from '../redux'
-import InfiniteScroll from "react-infinite-scroll-component";
-import Spinner from 'react-bootstrap/Spinner'
 
 import CryptoCard from './CryptoCard';
 
 function CryptosContainer({ cryptoData, fetchCryptos }) {
-
   useEffect(() => {
-    fetchCryptos()
+    fetchCryptos();
+    window.addEventListener("scroll", handleScroll);
   }, [])
+  const handleScroll = (e) => {
+    console.log(e.target.documentElement.scrollTop);
+    console.log(e.target.documentElement.scrollHeight);
+    console.log(window.innerHeight)
+    if (window.innerHeight + e.target.documentElement.scrollTop + 1 >= e.target.documentElement.scrollHeight) {
+      fetchCryptos();
+    }
+  };
   return (
     <div style={{ backgroundColor: '#080d12' }}>
-      <InfiniteScroll
-        dataLength={cryptoData.cryptos.length}
-        next={fetchCryptos}
-        hasMore={true}
-        loader={<Spinner animation="grow" />}
-        endMessage={
-          <p style={{ textAlign: 'center' }}>
-            <b>Yay! You have seen it all</b>
-          </p>
-        }
-      >
-        {cryptoData &&
-          cryptoData.cryptos &&
-          cryptoData.cryptos.map((crypto, index) => <CryptoCard key={index} crypto={crypto}></CryptoCard>)}
-      </InfiniteScroll>
+      {cryptoData &&
+      cryptoData.cryptos &&
+      cryptoData.cryptos.map((crypto, index) => <CryptoCard key={index} crypto={crypto}></CryptoCard>)}
     </div>
+    
   )
 }
 
